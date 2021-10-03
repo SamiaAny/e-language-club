82e96eecaa8c47567da28246a22025f049a18f44
SamiaAny <nstu.any@gmail.com>
2021-10-04 03:10:42 +0600

    add header & footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p className="text-center">&copy;copyright reserved by E-Language-Club </p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <div className="banner">
+                <div className="banner-details text-center pt-5">
+                    <h1>Explore New Skills,<br/>Learn Poplar Langages</h1>
+                    <p>Choose from hundred of expert-led courses now</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
